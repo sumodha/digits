cb57283a485fadf55c588c349867a0d615d620a4
sumodha <pokhrel.sumodha@gmail.com>
2025-09-04 00:55:33 -0500

    testing...

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT
 
 const app = express();
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 await connectDB();
 const collection = await getCollection();

ace7d233242622aa7950c3485aaaaa3007abdb25
sumodha <pokhrel.sumodha@gmail.com>
2025-09-04 01:12:18 -0500

    fixed cors issue

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 await connectDB();
 const collection = await getCollection();
